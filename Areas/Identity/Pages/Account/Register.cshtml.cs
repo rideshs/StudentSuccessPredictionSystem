31e2ff15e9e21614f6724452c729c40db9723678
kewang <kewang.h.limbu@gmail.com>
2023-05-09 12:44:15 +0545

    Add A Default Role to Newly Registered User

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -145,6 +145,9 @@ namespace StudentSuccessPrediction.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    
+                    //Add A Default Role to Newly Registered User
+                    await _userManager.AddToRoleAsync(user, Enums.Roles.Student.ToString());
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
