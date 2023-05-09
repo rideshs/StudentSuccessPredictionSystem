6654d8837073505447e741224354c49a54e9d906
kewang <kewang.h.limbu@gmail.com>
2023-05-09 06:07:18 +0545

    Adding Custom Fields to Identity User

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using StudentSuccessPrediction.Models;
 
 namespace StudentSuccessPrediction.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StudentSuccessPrediction.Models
+{       
+    // change the default User class from IdentityUser to ApplicationUser
+
+    public class ApplicationUser: IdentityUser
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int UsernameChangeLimit { get; set; } = 3;
+        public byte[]? ProfilePicture { get; set; }
+    }
+}
